b01d3132f86d0a84c5287afdfef6c926fe8c36ed
reciliencia_clothes3 <83990977+lucasDuet@users.noreply.github.com>
2022-06-29 20:07:43 -0300

    a

---
+++ components/formularios.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+export default function formularios(){
+    return <h2>Login</h2>;
+}
--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 function App() {
   return (
    <div className='Formulario'>
-   </div>
-        
+    <formularios/>
+
       
+   </div>
+     
   );
 }
 
