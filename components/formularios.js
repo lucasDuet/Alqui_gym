508bffb1e9707b903d41eb43fd6e6c88e75cc43c
reciliencia_clothes3 <83990977+lucasDuet@users.noreply.github.com>
2022-07-18 20:32:41 -0300

    n

--- components/formularios.js
+++ components/formularios.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+
 export default function formularios(){
-    return <h2>Login</h2>;
+    return (
+        <>
+    <h2>Login</h2>
+    <form>
+<label for="">Nombre:</label>
+<input type="text" id="nombre"/>
+    </form>
+    </>
+    );
 }
